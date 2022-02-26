aeb77b78fc9c81a3d8338393970929511216716c
EricBier <ericfrancax96@gmail.com>
2022-02-25 21:57:59 -0300

    Exercicios JavaScript, curso em video, criando tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Ola!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let seltab = document.getElementById('seltab')
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        seltab.innerHTML = ''
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab ${c}`
+            seltab.appendChild(item) 
+            c++
+        }
+    }
+}
